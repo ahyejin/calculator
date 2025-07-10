246bf2c1ced544df46ef057ca1594823262abd91
ahyejin <anishakj8293@gmail.com>
2025-07-10 10:12:02 +0530

    Handle the case of division by zero

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const allButtons = document.querySelector(".allButtons");
 let operatorFlag = false;
 let lastPressed = "";
 allButtons.addEventListener("click", function (event) {
+    
     let entry = event.target.textContent;
 
     if (entry === "ac") {
         input.textContent = "";
         result.textContent = "";
         operatorFlag = false;
+        return;
         
     }
     if (entry === "bs") {
         input.textContent = input.textContent.substring(0, input.textContent.length - 1);
         result.textContent = "";
+        result.style.color = "black";
+
         console.log(input.textContent);
         const matchExpression = input.textContent.match(/^(\d+)$/);
         console.log(matchExpression);
@@ -26,6 +30,7 @@ allButtons.addEventListener("click", function (event) {
     }
     if (Number.isInteger(+entry)) {
         input.textContent += entry;
+        result.style.color = "black";
         operatorFlag = true;
     }
     if (operatorFlag) {
@@ -37,7 +42,7 @@ allButtons.addEventListener("click", function (event) {
     const expression = input.textContent;
     const array = expression.match(/^(\d+)([-+*/%^])(\d+)$/);
     if (array) {
-       // console.log(array);
+        console.log(array);
         operate(array);
         operatorFlag = false;
     }
@@ -69,8 +74,6 @@ function operate(input) {
 const add = function (a, b) {
     a = (+a);
     b = (+b);
-    //console.log(a);
-    //console.log(b);
     let ans = a + b;
     result.textContent = ans;
 }
@@ -89,6 +92,11 @@ const multiply = function (a, b) {
 const divison = function (a, b) {
     a = (+a);
     b = (+b);
+    if(b == 0){
+        result.textContent = "Can't divide by 0";
+        result.style.color = "red";
+        return;
+    }
     let ans = a / b;
     result.textContent = ans;
 }
@@ -101,6 +109,11 @@ const power = function (a, b) {
 const percentage = function (a, b) {
     a = (+a);
     b = (+b);
+    if(b == 0){
+        result.textContent = "Can't divide by 0";
+        result.style.color = "red";
+        return;
+    }
     let ans = (a / b) * 100;
     result.textContent = ans;
 }
